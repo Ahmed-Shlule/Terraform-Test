447cd571354e7a1a085fb50cb5259172925940d6
Ahmed Shlule <Ahmed.Shlule@ICTOasis.co.uk>
2019-01-18 15:21:30 +0000

    Added index.html

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = ">= 0.8, < 0.11"
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
@@ -6,6 +10,12 @@ resource "aws_instance" "example" {
   ami		= "ami-01419b804382064e4"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+              
   tags {
     Name = "terraform-example"
   }
