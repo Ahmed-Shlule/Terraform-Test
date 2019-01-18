352e618e23e629b1104b7af6542ba68676c36ac0
Ahmed Shlule <Ahmed.Shlule@ICTOasis.co.uk>
2019-01-18 15:34:47 +0000

    Added vpc_security_group_id

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami		= "ami-01419b804382064e4"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
